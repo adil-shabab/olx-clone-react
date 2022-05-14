e28098017fd33303bca4bbfc411a0d2f25bce884
Adil Shabab <adilshabab2004@gmail.com>
2022-05-14 17:06:58 +0530

    bootstrap navbar added

--- Olx/olx/src/App.js
+++ Olx/olx/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Home from './Pages/Home';
 
 function App() {
   return (
     <div className="App">
-      
+      <Home/>
     </div>
   );
 }
--- Olx/olx/src/Pages/Home.js
+++ Olx/olx/src/Pages/Home.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Header from '../Components/Header/Header'
 
 function Home() {
   return (
     <div>
-        Home
+        <Header/>
     </div>
   )
 }
