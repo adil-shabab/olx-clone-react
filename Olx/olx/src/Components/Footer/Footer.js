5ce679e374768219ae17204bfa6b121c41aa73f6
Adil Shabab <adilshabab2004@gmail.com>
2022-05-15 15:28:14 +0530

    social media icons added too footer

--- Olx/olx/src/Components/Footer/Footer.js
+++ Olx/olx/src/Components/Footer/Footer.js
@@ -18,6 +18,58 @@ function Footer() {
             </ul>
           </div>
       </div>
+      <div className="content container-fluid pt-3 col-lg-2 col-md-3 col-sm-6">
+          <div className="heading">
+            <p>TRENDING LOCATIONS</p>
+          </div>
+          <div className="list">
+            <ul>
+              <li>Bhubaneshwar</li>
+              <li>Hyderabad</li>
+              <li>Chandigarh</li>
+              <li>Nashik</li>
+            </ul>
+          </div>
+      </div>
+      <div className="content container-fluid pt-3 col-lg-2 col-md-3 col-sm-6">
+          <div className="heading">
+            <p>ABOUT US</p>
+          </div>
+          <div className="list">
+            <ul>
+              <li>About OLX Group</li>
+              <li>Careers</li>
+              <li>Contact Us</li>
+              <li>Waah Jobs</li>
+            </ul>
+          </div>
+      </div>
+      <div className="content container-fluid pt-3 col-lg-2 col-md-3 col-sm-6">
+          <div className="heading">
+            <p>OLX</p>
+          </div>
+          <div className="list">
+            <ul>
+              <li>Help</li>
+              <li>Sitemap</li>
+              <li>Legal & Privacy Information</li>
+              <li>Blog</li>
+            </ul>
+          </div>
+      </div>
+      <div className="content container-fluid pt-3 col-lg-2 col-md-3 col-sm-6">
+          <div className="heading">
+            <p>FOLLOW US</p>
+          </div>
+          <div className="list">
+            <ul className='d-flex'>
+              <li><i class="fa-brands fa-facebook"></i></li>
+              <li><i class="fa-brands fa-instagram"></i></li>
+              <li><i class="fa-brands fa-twitter"></i></li>
+              <li><i class="fa-solid fa-play"></i></li>
+            </ul>
+          </div>
+      </div>
     </div>
     <div className="footer">
     <p>Other Countries Pakistan - South Africa - Indonesia</p>
