2b9a70a28ca3d5f6ae906d45434db1a4a7a241cb
Adil Shabab <adilshabab2004@gmail.com>
2022-05-15 15:05:41 +0530

    all set in post

--- Olx/olx/src/Components/Post/Post.js
+++ Olx/olx/src/Components/Post/Post.js
@@ -7,85 +7,13 @@ function Post() {
   return (
     <div className="postParentDiv">
       <div className="moreView">
-        <div className="heading">
+        <div className="heading d-flex align-items-center">
           <span>Quick Menu</span>
           <span>View more</span>
         </div>
         <div className="cards">
           <div className="card col-lg-3 col-md-4 col-sm-6 col-xsm-12">
-            <div className="p-3 bg-white shadow w-100">
-              <div className="favorite">
-                <Heart></Heart>
-              </div>
-              <div className="image">
-                <img src={bike} alt="" />
-              </div>
-              <div className="content">
-                <p className="rate">&#x20B9; 250000</p>
-                <span className="kilometer">Two Wheeler</span>
-                <p className="name"> YAMAHA R15V3</p>
-              </div>
-              <div className="date">
-                <span>Tue May 04 2021</span>
-              </div>
-            </div>
-          </div>
-          <div className="card col-lg-3 col-md-4 col-sm-6 col-xsm-12">
-            <div className="p-3 bg-white shadow w-100">
-              <div className="favorite">
-                <Heart></Heart>
-              </div>
-              <div className="image">
-                <img src={bike} alt="" />
-              </div>
-              <div className="content">
-                <p className="rate">&#x20B9; 250000</p>
-                <span className="kilometer">Two Wheeler</span>
-                <p className="name"> YAMAHA R15V3</p>
-              </div>
-              <div className="date">
-                <span>Tue May 04 2021</span>
-              </div>
-            </div>
-          </div>
-          <div className="card col-lg-3 col-md-4 col-sm-6 col-xsm-12">
-            <div className="p-3 bg-white shadow w-100">
-              <div className="favorite">
-                <Heart></Heart>
-              </div>
-              <div className="image">
-                <img src={bike} alt="" />
-              </div>
-              <div className="content">
-                <p className="rate">&#x20B9; 250000</p>
-                <span className="kilometer">Two Wheeler</span>
-                <p className="name"> YAMAHA R15V3</p>
-              </div>
-              <div className="date">
-                <span>Tue May 04 2021</span>
-              </div>
-            </div>
-          </div>
-          <div className="card col-lg-3 col-md-4 col-sm-6 col-xsm-12">
-            <div className="p-3 bg-white shadow w-100">
-              <div className="favorite">
-                <Heart></Heart>
-              </div>
-              <div className="image">
-                <img src={bike} alt="" />
-              </div>
-              <div className="content">
-                <p className="rate">&#x20B9; 250000</p>
-                <span className="kilometer">Two Wheeler</span>
-                <p className="name"> YAMAHA R15V3</p>
-              </div>
-              <div className="date">
-                <span>Tue May 04 2021</span>
-              </div>
-            </div>
-          </div>
-          <div className="card col-lg-3 col-md-4 col-sm-6 col-xsm-12">
-            <div className="p-3 bg-white shadow w-100">
+            <div className="p-3 bg-white shadow w-100 card-child">
               <div className="favorite">
                 <Heart></Heart>
               </div>
