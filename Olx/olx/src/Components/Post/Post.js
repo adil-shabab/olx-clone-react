911df832b8f033af24ab2ec9a0ecdabca39d8829
Adil Shabab <adilshabab2004@gmail.com>
2022-05-15 15:07:58 +0530

    post section completed

--- Olx/olx/src/Components/Post/Post.js
+++ Olx/olx/src/Components/Post/Post.js
@@ -32,29 +32,6 @@ function Post() {
           </div>
         </div>
       </div>
-      {/* <div className="recommendations">
-      <div className="heading">
-        <span>Fresh recommendations</span>
-      </div>
-      <div className="cards">
-        <div className="card">
-          <div className="favorite">
-            <Heart></Heart>
-          </div>
-          <div className="image">
-            <img src={bike} alt="" />
-          </div>
-          <div className="content">
-            <p className="rate">&#x20B9; 250000</p>
-            <span className="kilometer">Two Wheeler</span>
-            <p className="name"> YAMAHA R15V3</p>
-          </div>
-          <div className="date">
-            <span>10/5/2021</span>
-          </div>
-        </div>
-      </div>
-    </div> */}
     </div>
   );
 }
