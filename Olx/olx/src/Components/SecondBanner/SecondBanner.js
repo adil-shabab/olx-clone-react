db9620bad9ad550693d26bb129f7f1fba1f51145
Adil Shabab <adilshabab2004@gmail.com>
2022-05-15 22:10:11 +0530

    second banner updating

--- Olx/olx/src/Components/SecondBanner/SecondBanner.js
+++ Olx/olx/src/Components/SecondBanner/SecondBanner.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import './SecondBanner.css'
+import BannerImg from '../../Images/b16.jpg'
+import Playstore from '../../Images/play.jpg'
+import Appstore from '../../Images/app.jpg'
+
 
 function SecondBanner() {
   return (
-    <div>
-        <img src='https://i.pinimg.com/originals/c6/b7/8e/c6b78ed22b30a8230f5ccea2ddc88468.jpg' alt='second banner'/>
+    <div className='container-fluid row p-0'>
+        <div className='col-xlg-8 col-lg-8 col-md-8 col-sm-8 col-12'>
+        <img className='w-100' src={BannerImg} alt='second banner'/>
+        </div>
+        <div className='download col-xlg-4 col-lg-4 col-md-4 col-sm-4 col-4'>
+            <h5>Download App</h5>
+            <img src={Playstore} alt='play store icon'/>
+            <img src={Appstore} alt='app store icon'/>
+        </div>
     </div>
   )
 }
