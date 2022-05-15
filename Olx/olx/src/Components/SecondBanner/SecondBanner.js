8691c23ce30c38ae1bb748e279e432c9f41023a8
Adil Shabab <adilshabab2004@gmail.com>
2022-05-15 21:38:58 +0530

    second banner added

---
+++ Olx/olx/src/Components/SecondBanner/SecondBanner.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './SecondBanner.css'
+
+function SecondBanner() {
+  return (
+    <div>
+        <img src='https://i.pinimg.com/originals/c6/b7/8e/c6b78ed22b30a8230f5ccea2ddc88468.jpg' alt='second banner'/>
+    </div>
+  )
+}
+
+export default SecondBanner
--- Olx/olx/src/Pages/Home.js
+++ Olx/olx/src/Pages/Home.js
@@ -4,6 +4,7 @@ import BannerImage from '../Components/BannerImage/BannerImage'
 import Footer from '../Components/Footer/Footer'
 import Header from '../Components/Header/Header'
 import Post from '../Components/Post/Post'
+import SecondBanner from '../Components/SecondBanner/SecondBanner'
 
 function Home() {
   return (
@@ -12,6 +13,7 @@ function Home() {
       <Banner/>
       <BannerImage/>
       <Post/>
+      <SecondBanner/>
       <Footer/>
     </div>
   )
