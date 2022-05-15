990c2c7f4d4796277f1c36096e75a4ff659eaf1d
Adil Shabab <adilshabab2004@gmail.com>
2022-05-15 11:46:31 +0530

    banner section header completed

--- Olx/olx/src/Components/Banner/Banner.js
+++ Olx/olx/src/Components/Banner/Banner.js
@@ -1,23 +1,24 @@
 import React from 'react'
 import './Banner.css'
-import Arrow from '../../assets/Arrow'
+// import Arrow from '../../assets/Arrow'
 function Banner() {
   return (
-    <div className="container=fluid banner-container">
-      <div className="bannerChildDiv">
+    <div className="banner-container container">
+      <div className="banner-child">
         <div className="menuBar">
           <div className="categoryMenu row">
             <span>ALL CATEGORIES</span>
-            <Arrow></Arrow> 
+            <i className="fa-solid fa-angle-down pt-1 arrow"></i> 
           </div>
-          <div className="otherQuickOptions ml-3">
+          <div className="options ml-3">
             <span>Cars</span>
             <span>Motorcycle</span>
             <span>Smart Phone</span>
             <span>Apartment</span>
             <span>Scooter</span>
-            <span>Commercial</span>
             <span>House</span>
+            <span>Rent House</span>
+            <span>Smart Watch</span>
           </div>
         </div>
         <div className="banner">
