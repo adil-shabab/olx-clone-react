72249c44f1784a16dfdcdca0683d702d966c008e
Adil Shabab <adilshabab2004@gmail.com>
2022-05-17 16:20:29 +0530

    create page progressing

--- Olx/olx/src/Components/CreatePage/CreatePage.js
+++ Olx/olx/src/Components/CreatePage/CreatePage.js
@@ -1,15 +1,27 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './CreatePage.css'
 
 function CreatePage() {
+
+  const [name, setName] = useState("");
+  const [category, setCategory] = useState("");
+  const [price, setPrice] = useState();
+
+
   return (
     <card>
         <div className="create-container">
-          <form>
+          <form onSubmit={(e)=>{
+            e.preventDefault()
+          }}>
             <label htmlFor="fname">Name</label>
             <br />
             <input
               className="input"
+              value={name}
+              onChange={(e)=>{
+                setName(e.target.value)
+              }}
               type="text"
               id="fname"
               name="Name"
@@ -21,22 +33,30 @@ function CreatePage() {
             <input
               className="input"
               type="text"
+              value={category}
               id="fname"
+              onChange={(e)=>{
+                setCategory(e.target.value)
+              }}
               name="category"
             />
             <br />
-            <label htmlFor="fname">Price</label>
+            <label htmlFor="fname"
+            >Price</label>
             <br />
-            <input className="input" type="number" id="fname" name="Price" />
+            <input className="input" value={price} onChange={(e)=>{
+              setPrice(e.target.value)
+            }} type="number" id="fname" name="Price" />
             <br />
-          </form>
+          
           <br />
-          <img alt="Posts" width="200px" height="200px" src="" className='mb-2'></img>
-          <form>
+          <img alt="Posts" width="200px" height="200px" src='' className='mb-2'></img>
+          
             <br />
-            <input type="file" />
+            <input
+            type="file" />
             <br />
-            <button className="uploadBtn">upload and Submit</button>
+            <button type='submit' className="uploadBtn">upload and Submit</button>
           </form>
         </div>
       </card>
